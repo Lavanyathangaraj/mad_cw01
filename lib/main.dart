5f6acc7fa287726d7229391c560cc696056c250b
Lavanya Thangaraj <lavanyathangaraj789@gmail.com>
2025-09-20 17:40:48 -0400

    switch between light and dark themes

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,68 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _darkMode = false;
+
+  void _toggleTheme() {
+    setState(() {
+      _darkMode = !_darkMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: _darkMode
+          ? ThemeData.dark().copyWith(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.green))
+          : ThemeData.light().copyWith(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.green)),
+      home: MyHomePage(toggleTheme: _toggleTheme),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key, required this.toggleTheme});
+  final VoidCallback toggleTheme;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   int _counter = 0;
   bool _initialImage = true;
+
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -38,8 +74,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _toggleImage() {
-    setState(() {
-      _initialImage = !_initialImage;
+    _controller.reverse().then((_) {
+      setState(() {
+        _initialImage = !_initialImage;
+      });
+      _controller.forward();
     });
   }
 
@@ -47,13 +86,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Counter & Image Toggle"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
@@ -65,16 +103,24 @@ class _MyHomePageState extends State<MyHomePage> {
               child: const Text("Increment"),
             ),
             const SizedBox(height: 30),
-            Image.asset(
-              _initialImage ? 'assets/image1.png' : 'assets/image2.png',
-              height: 200,
-              width: 200,
-              fit: BoxFit.cover,
+            FadeTransition(
+              opacity: _animation,
+              child: Image.asset(
+                _initialImage ? 'assets/image1.png' : 'assets/image2.png',
+                height: 300,
+                width: 350,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _toggleImage,
               child: const Text("Toggle Image"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: widget.toggleTheme,
+              child: const Text("Toggle Theme"),
             ),
           ],
         ),
